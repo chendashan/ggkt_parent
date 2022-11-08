eccf1bcd308cd1718eae1fce66ca7fd0fef021cb
chendashan <cds7777@163.com>
2022-11-09 00:28:11 +0800

    讲师增删改查

---
+++ common/service_utils/src/main/java/com/example/ggkt/result/Result.java
@@ -0,0 +1,51 @@
+package com.example.ggkt.result;
+
+import lombok.Data;
+
+@Data
+public class Result<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    public Result() {
+    }
+
+    public static <T> Result<T> ok() {
+        return ok(null);
+    }
+
+    public static <T> Result<T> fail() {
+        return fail(null);
+    }
+
+    public static <T> Result<T> ok(T data) {
+        Result<T> result = new Result<>();
+        if (data != null) {
+            result.setData(data);
+        }
+        result.setCode(200);
+        result.setMessage("成功");
+        return result;
+    }
+
+    public static <T> Result<T> fail(T data) {
+        Result<T> result = new Result<>();
+        if (data != null) {
+            result.setData(data);
+        }
+        result.setCode(201);
+        result.setMessage("失败");
+        return result;
+    }
+
+    public Result<T> message(String message) {
+        this.setMessage(message);
+        return this;
+    }
+
+    public Result<T> code(Integer code) {
+        this.setCode(code);
+        return this;
+    }
+}
--- service/service_vod/src/main/java/com/example/ggkt/vod/controller/TeacherController.java
+++ service/service_vod/src/main/java/com/example/ggkt/vod/controller/TeacherController.java
@@ -2,8 +2,15 @@ package com.example.ggkt.vod.controller;
 
 
 import com.atguigu.ggkt.model.vod.Teacher;
+import com.atguigu.ggkt.vo.vod.TeacherQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.ggkt.result.Result;
 import com.example.ggkt.vod.service.TeacherService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,15 +33,91 @@ public class TeacherController {
 
     //http://localhost:8301/admin/vod/teacher/findAll
     @GetMapping("findAll")
-    public List<Teacher> findAllTeacher() {
+    public Result findAllTeacher() {
         List<Teacher> list = teacherService.list();
-        return list;
+        return Result.ok(list).message("查询数据成功");
     }
 
     @DeleteMapping("remove/{id}")
-    public boolean removeTeacher(@PathVariable Long id) {
+    public Result removeTeacher(@PathVariable Long id) {
         boolean isSuccess = teacherService.removeById(id);
-        return isSuccess;
+        if (isSuccess) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("讲师条件查询分页")
+    @PostMapping("queryTeacherPage/{current}/{limit}")
+    public Result findPage(@PathVariable long current, @PathVariable long limit, @RequestBody(required = false) TeacherQueryVo teacherQueryVo) {
+        Page<Teacher> pageParam = new Page<>(current, limit);
+        if (teacherQueryVo == null) {
+            IPage<Teacher> pageModel = teacherService.page(pageParam, null);
+            return Result.ok(pageModel);
+        } else {
+            String name = teacherQueryVo.getName();
+            Integer level = teacherQueryVo.getLevel();
+            String joinDateBegin = teacherQueryVo.getJoinDateBegin();
+            String joinDateEnd = teacherQueryVo.getJoinDateEnd();
+            QueryWrapper<Teacher> queryWrapper = new QueryWrapper<>();
+            if (!StringUtils.isEmpty(name)) {
+                queryWrapper.like("name", name);
+            }
+            if (!StringUtils.isEmpty(level)) {
+                queryWrapper.eq("level", name);
+            }
+            if (!StringUtils.isEmpty(joinDateBegin)) {
+                queryWrapper.ge("join_date", joinDateBegin);
+            }
+            if (!StringUtils.isEmpty(joinDateEnd)) {
+                queryWrapper.le("join_date", joinDateEnd);
+            }
+
+            IPage<Teacher> pageModel = teacherService.page(pageParam, queryWrapper);
+            return Result.ok(pageModel);
+        }
+
+    }
+
+    @ApiOperation("添加讲师")
+    @PostMapping("addTeacher")
+    public Result addTeacher(@RequestBody Teacher teacher) {
+        boolean isSuccess = teacherService.save(teacher);
+        if (isSuccess) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("根据id查询")
+    @GetMapping("getById/{id}")
+    public Result getById(@PathVariable Long id) {
+        Teacher teacher = teacherService.getById(id);
+        return Result.ok(teacher);
+    }
+
+    @ApiOperation("修改讲师信息")
+    @PostMapping("updateTeacher")
+    public Result updateTeacher(@RequestBody Teacher teacher) {
+        boolean isSuccess = teacherService.updateById(teacher);
+        if (isSuccess) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @ApiOperation("批量删除讲师")
+    @DeleteMapping("deleteBatch")
+    public Result deleteBatch(@RequestBody List<Long> ids) {
+        boolean isSuccess = teacherService.removeByIds(ids);
+        if (isSuccess) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
     }
 
 }
