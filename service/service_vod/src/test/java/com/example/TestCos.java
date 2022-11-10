fe87277e53324cbf23bd22e566f2a43afcf25e6d
chendashan <cds7777@163.com>
2022-11-11 01:52:18 +0800

    处理

--- service/service_vod/src/test/java/com/example/TestCos.java
+++ service/service_vod/src/test/java/com/example/TestCos.java
@@ -17,8 +17,8 @@ public class TestCos {
     public static void main(String[] args) {
         // 1 初始化用户身份信息（secretId, secretKey）。
         // SECRETID和SECRETKEY请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi 进行查看和管理
-        String secretId = "AKIDAJNEzeEjOPI90ogYOKGEyfG3l1MdLdTe";
-        String secretKey = "AHh8bx7GFC4oT4UxLZgcDKH5qQvLahAS";
+        String secretId = "我的SECRETID";
+        String secretKey = "我的SECRETKEY";
         COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
         // 2 设置 bucket 的地域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
         // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
